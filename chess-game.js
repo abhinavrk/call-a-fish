3eb027c4641c71010e7bd12b3356ec5120cdc941
Abhinav Ramakrishnan <abhinavrk@users.noreply.github.com>
2023-01-22 15:54:54 -0500

    making the board draggable

--- chess-game.js
+++ chess-game.js
@@ -4,6 +4,8 @@ var game = new Chess();
 // Create a new chess board
 var board = ChessBoard('chess-board', {
   position: game.fen(), // Use the FEN from the chess.js game
+  draggable: true,
+  droffOffBoard: 'snapback',
   onDrop: function(source, target) {
     // Make the move in the chess.js game
     var move = game.move({
