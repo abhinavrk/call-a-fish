9126f13694f404a257adc4ce887c1c529e4d01f6
Abhinav Ramakrishnan <abhinavrk@users.noreply.github.com>
2023-01-22 16:27:52 -0500

    updated chess logic

--- chess-game.js
+++ chess-game.js
@@ -1,25 +1,53 @@
 "use strict";
-var game = new Chess();
 
-// Create a new chess board
-var board = ChessBoard('chess-board', {
-  position: game.fen(), // Use the FEN from the chess.js game
-  draggable: true,
-  droffOffBoard: 'snapback',
-  onDrop: function(source, target) {
-    // Make the move in the chess.js game
-    var move = game.move({
-      from: source,
-      to: target,
-      promotion: 'q' // Always promote to a queen
-    });
+function createGame() {
+  return new Chess();
+}
 
-    // If the move was illegal, take back the piece
-    if (move === null) {
+function createBoard(game, isWhite, onDrop, boardId) {
+  return ChessBoard(boardId, {
+    position: game.fen(), // Use the FEN from the chess.js game
+    draggable: true,
+    droffOffBoard: 'snapback',
+    orientation: isWhite ? 'white' : 'black',
+    onDrop: onDrop,
+    sparePieces: true,
+  });
+}
+
+function onDrop(game, board, source, target) {
+  // Make the move in the chess.js game
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // Always promote to a queen
+  });
+
+  // If the move was illegal, take back the piece
+  if (move === null) {
+    return 'snapback';
+  }
+
+  // Update the board position
+  board.position(game.fen());
+}
+
+function setup(isWhite) {
+  var game = createGame();
+
+  var callback = function (source, target) {
+    var whiteToMove = game.turn() === 'w';
+    console.log(game.turn(), isWhite, whiteToMove);
+    if (whiteToMove === isWhite) {
+      onDrop(game, board, source, target);
+    } else {
+      alert('Not your move!');
       return 'snapback';
     }
-
-    // Update the board position
-    board.position(game.fen());
   }
-});
+
+  var board = createBoard(game, isWhite, callback, 'chess-board');
+
+}
+
+setup(true);
