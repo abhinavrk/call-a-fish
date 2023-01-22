e483576766b2fde505bcfc204a3e4cc62bdb78f1
KerimovEmil <emilkerimov93@gmail.com>
2023-01-22 15:21:05 -0500

    chess board js

---
+++ chess-game.js
@@ -0,0 +1,23 @@
+"use strict";
+var game = new Chess();
+
+// Create a new chess board
+var board = ChessBoard('chess-board', {
+  position: game.fen(), // Use the FEN from the chess.js game
+  onDrop: function(source, target) {
+    // Make the move in the chess.js game
+    var move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q' // Always promote to a queen
+    });
+
+    // If the move was illegal, take back the piece
+    if (move === null) {
+      return 'snapback';
+    }
+
+    // Update the board position
+    board.position(game.fen());
+  }
+});
